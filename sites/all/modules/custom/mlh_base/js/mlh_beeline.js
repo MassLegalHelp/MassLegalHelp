b6b1af20e4e8156e5a7d870bab86ad9f9df194d4
Kristen Pol <kristen.pol@gmail.com>
2016-07-10 13:50:25 -0700

    Update beeline reader example js comments.

--- sites/all/modules/custom/mlh_base/js/mlh_beeline.js
+++ sites/all/modules/custom/mlh_base/js/mlh_beeline.js
@@ -3,9 +3,10 @@
   Drupal.behaviors.beeline = {
     attach: function (context, settings) {
 
+      // Example of how to use the beeline reader library.
       var elements = document.querySelectorAll(".views-field-body p");
       for (var i = 0; i < elements.length; i++) {
-        // Possible themes: 'bright', 'dark', 'blues', 'gray', 'night_gray', and 'night_blues'
+        // Possible themes: bright, dark, blues, gray, night_gray, night_blues.
         var beeline = new BeeLineReader(elements[i], { theme: "bright" });
         beeline.color();
       }
